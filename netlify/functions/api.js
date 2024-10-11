fa6c5e4ba3728e120f437f2a6fc923c8571900ae
Ojiromayo <labeodanojiromayo@gmail.com>
2024-10-11 10:12:32 +0100

    Deployment

---
+++ netlify/functions/api.js
@@ -0,0 +1,101 @@
+const express = require("express")
+const app = express()
+const morgan = require("morgan")
+const mongoose = require("mongoose")
+require("dotenv/config")
+const port = process.env.PORT
+const session = require("express-session");
+const MongoStore = require('connect-mongo');
+const methodOverride = require('method-override')
+const Service = require("./models/services.js")
+const serverless = require('serverless-http')
+
+
+
+// ! MIDDLEWEAR FUNCTIONS
+const passUserToView = require("../../middleware/pass-user-to-view.js")
+const checkIfSignedIn = require("../../middleware/checkIfSignedIn")
+
+
+
+// ! CONTROLLERS
+const authRouter = require("../../controllers/auth")
+const serviceRouter = require("../../controllers/services")
+const usersrouter = require("../../controllers/users.js")
+
+
+
+
+
+// ! MIDDLEWEAR
+app.set("view engine", "ejs")
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'))
+app.use(express.static('public'));
+app.use('/node_modules', express.static('node_modules'));
+app.use(morgan("dev"))
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({
+        mongoUrl: process.env.MONGODB_URI,
+    }),
+}))
+app.use(passUserToView);
+
+const handleError = (res, error, message = "Internal Server Error", statusCode = 500) => {
+    console.error(error);
+    res.status(statusCode).render("error", { message }); // Assuming you have an 'error.ejs' page
+};
+
+
+
+// ! ROUTE HANDLERS
+
+// Landing Page
+app.get("/", async (req, res) => {
+    try {
+        const services = await Service.find();
+        // Slice the services array to get only the first 4 services
+        const limitedServices = services.slice(0, 4);
+        res.status(200).render("index", { services: limitedServices });
+    } catch (error) {
+        handleError(res, error);
+    }
+});
+
+
+// Routes
+app.use("/auth", authRouter);
+app.use("/services", checkIfSignedIn, serviceRouter);
+app.use("/user", checkIfSignedIn, usersrouter)
+
+
+
+
+
+// ! 404 Handling
+app.get("*", (req, res) => {
+    res.status(404).render("404")
+})
+
+
+
+// ! Start Server
+const startServer = async() => {
+    try {
+        // Connect to Database
+        await mongoose.connect(process.env.MONGODB_URI)
+        console.log("Connected to DB")
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
+startServer()
+
+
+module.exports.handler = serverless(app)
